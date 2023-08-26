470d1a930a5a5b66ff160d0b54fc331ae170d440
xuxail <81913083+xuxail@users.noreply.github.com>
2023-08-26 23:26:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   return (
     <div className="w-screen h-screen bg-blue-100 flex flex-col">
       <Routes>
+        <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/subscription" element={<Subscription />} />
